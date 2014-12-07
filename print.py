551bfc321a0bd6701fda8c4e849569a77bccc542
Matt Hampel <matth@localdata.com>
2014-12-07 11:13:46 -0500

    First commit'

---
+++ print.py
@@ -0,0 +1,40 @@
+# https://code.google.com/p/python-escpos/wiki/Usage
+
+# lsub
+# http://stackoverflow.com/questions/17058134/is-there-an-equivalent-of-lsusb-for-os-x
+# Bus 020 Device 001: ID 0519:0001 Star Micronics Co., LTD Composite Device
+#
+# 2. You'll need libusb for mac
+# Hard to find
+# http://www.ellert.se/twain-sane/
+#
+# 3. Then install http://walac.github.io/pyusb/
+#
+# 4. Then sudo pip install python-escpos
+
+#       Host Controller Location: Built-in USB
+#       Host Controller Driver: AppleUSBXHCI
+#       PCI Device ID: 0x1e31
+#       PCI Revision ID: 0x0004
+#       PCI Vendor ID: 0x8086
+#       Bus Number: 0x0a
+#
+#         Composite Device:
+#
+#           Product ID: 0x0001
+#           Vendor ID: 0x0519  (Star Micronics Co., LTD)
+#           Version:  2.01
+#           Speed: Up to 12 Mb/sec
+#           Location ID: 0x14300000 / 1
+#           Current Available (mA): 500
+#           Current Required (mA): 10
+#           1284 Device ID: MFG:Star;CMD:STAR;MDL:TSP600 (STR_T-U001);CLS:PRINTER;
+
+import epsonprinter
+
+
+printer = epsonprinter.EpsonPrinter(0x0519,0x0001)
+
+printer.print_text("Hello, how's it going?")
+printer.linefeed()
+
